503cfa9b4c0202be237b7b7afd8e929933236f8c
deedeeh <dinahafez.8@gmail.com>
2019-05-18 12:24:25 +0100

    Created a post request and installed express-formidable

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require('express');
+const formidable = require('express-formidable');
 const app = express();
+
+app.use(express.static('public'));
+app.use(formidable());
+
+app.post('/create-post', (req, res) => {
+  console.log(req.fields);
+});
 
 app.listen(3000, () => {
   console.log('Server is litening on port 3000. ready to accept requests!');
 });
+
