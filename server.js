d12734ff2a8ac48abb1f3eb5bdf4a5bb670038e8
deedeeh <dinahafez.8@gmail.com>
2019-05-23 23:00:57 +0100

    Added the correct JSON format

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const formidable = require('express-formidable');
+const fs = require('fs');
 const app = express();
 
 app.use(express.static('public'));
 app.use(formidable());
 
 app.post('/create-post', (req, res) => {
-  console.log(req.fields);
+  let dataFormat = {}
+  dataFormat[Date.now()] = req.fields.blogpost
+  fs.writeFile('data/posts.json', JSON.stringify(dataFormat), (error) => {
+  });
 });
 
 app.listen(3000, () => {
